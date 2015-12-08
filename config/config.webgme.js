463eb8b0cc4af5234bc3573f99f7ffa81e9b7105
Brian Broll <brian.broll@vanderbilt.edu>
2015-12-08 15:32:28 -0600

    Added addon example

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -9,9 +9,10 @@ var config = require('webgme/config/config.default'),
 
 // The paths can be loaded from the webgme-setup.json
 config.plugin.basePaths.push('src/plugins');
+config.addOn.basePaths.push('src/addons');
 config.seedProjects.basePaths.push('src/seeds/NashJS');
 
-
+config.addOn.enable = true;
 // Visualizer descriptors
 
 // Add requirejs paths
